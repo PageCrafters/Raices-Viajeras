817434e81534cab21a47c1f9a9bd5fb7086a0243
TuNombreApellidos <manudatos1@gmail.com>
2025-12-03 13:13:47 +0100

    prueba Manu con validaciones

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,10 @@
+import {initializeFormController} from "./formController.js";
+
 const cambiaModoColor = () => {
     document.documentElement.classList.toggle('dark-mode');
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Inicializar el controlador del formulario
+    initializeFormController();
+})
