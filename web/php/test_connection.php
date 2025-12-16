1770ebb9f37b03ccd0dc878f9ee8289503145def
TuNombreApellidos <manudatos1@gmail.com>
2025-12-16 09:37:39 +0100

    db funcional

--- web/php/test_connection.php
+++ web/php/test_connection.php
@@ -2,6 +2,16 @@
 include 'dbconnect.php';
 if (isset($conn) && !$conn->connect_error) {
     echo 'Conexión exitosa a la base de datos.';
+    $query = "SELECT * FROM usuarios";
+    $result = $conn->query($query);
+    echo $result->num_rows . ' registros encontrados en la tabla usuarios.';
+    //insertar usuario de prueba
+    $insert_query = "INSERT INTO usuarios VALUES (NULL)";
+    if ($conn->query($insert_query) === TRUE) {
+        echo ' Usuario de prueba insertado correctamente.';
+    } else {
+        echo ' Error al insertar el usuario de prueba: ' . $conn->error;
+    }
 } else {
     echo 'Error de conexión: ' . $conn->connect_error;
 }
