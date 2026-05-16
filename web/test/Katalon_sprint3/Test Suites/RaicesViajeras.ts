088e95b3274f90dd9ddafbf86d02c238d252921f
Ana Lopez <159544230+analopez03@users.noreply.github.com>
2026-05-16 18:44:56 +0200

    añadido test de sprint 3

---
+++ web/test/Katalon_sprint3/Test Suites/RaicesViajeras.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RaicesViajeras</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>75c99564-b07e-4d7d-9446-7affdd335bfe</testSuiteGuid>
+   <testCaseLink>
+      <guid>9407af88-052b-400d-b835-8fd4a5b7fbfb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RaicesViajeras/Untitled Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
